4c3537bb4621be9730e62fb73f9b3f1114132c3b
Alex Wood <alexpwooddev@gmail.com>
2020-07-24 16:15:12 -0500

    first commit

---
+++ Data/CricApiHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketFavourites.Data
+{
+    public class CricApiHelper
+    {
+        public const string BASE_URL = "https://cricapi.com/api/";
+        public const string API_KEY = "?apikey=NbfhBv24s7REWqDADZzeRvGvsAF2";
+        public const string PLAYER_SEARCH_ENDPOINT = "playerFinder{0}&name={1}";
+        public const string PLAYER_ENDPOINT = "playerStats{0}&pid={1}";
+
+
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketFavourites.Models
+{
+    public class Root
+    {
+        public int pid { get; set; }
+        public string profile { get; set; }
+        public string imageURL { get; set; }
+        public string battingStyle { get; set; }
+        public string bowlingStyle { get; set; }
+        public string majorTeams { get; set; }
+        public string currentAge { get; set; }
+        public string born { get; set; }
+        public string fullName { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+        public string playingRole { get; set; }
+        public string v { get; set; }
+        public Data data { get; set; }
+        public int ttl { get; set; }
+        public Provider provider { get; set; }
+        public int creditsLeft { get; set; }
+
+    }
+
+
+    public class Data
+    {
+        public Bowling bowling { get; set; }
+        public Batting batting { get; set; }
+
+    }
+
+    public class Bowling
+    {
+        public ListA listA { get; set; }
+        public FirstClass firstClass { get; set; }
+        public T20Is T20Is { get; set; }
+        public ODIs ODIs { get; set; }
+        public Tests tests { get; set; }
+
+    }
+
+    public class Batting
+    {
+        public ListA2 listA { get; set; }
+        public FirstClass2 firstClass { get; set; }
+        public T20Is2 T20Is { get; set; }
+        public ODIs2 ODIs { get; set; }
+        public Tests2 tests { get; set; }
+
+    }
+
+   
+
+    public class Provider
+    {
+        public string source { get; set; }
+        public string url { get; set; }
+        public DateTime pubDate { get; set; }
+
+    }
+
+   
+
+
+
+
+    public class ListA
+    {
+        [DataMember(Name = "10")]
+        public string ten { get; set; }
+        [DataMember(Name = "5w")]
+        public string fiveW { get; set; }
+        [DataMember(Name = "4w")]
+        public string fourW { get; set; }
+        public string SR { get; set; }
+        public string Econ { get; set; }
+        public string Ave { get; set; }
+        public string BBM { get; set; }
+        public string BBI { get; set; }
+        public string Wkts { get; set; }
+        public string Runs { get; set; }
+        public string Balls { get; set; }
+        public string Inns { get; set; }
+        public string Mat { get; set; }
+
+    }
+
+    public class FirstClass
+    {
+        [DataMember(Name = "10")]
+        public string ten { get; set; }
+        [DataMember(Name = "5w")]
+        public string fiveW { get; set; }
+        [DataMember(Name = "4w")]
+        public string SR { get; set; }
+        public string Econ { get; set; }
+        public string Ave { get; set; }
+        public string BBM { get; set; }
+        public string BBI { get; set; }
+        public string Wkts { get; set; }
+        public string Runs { get; set; }
+        public string Balls { get; set; }
+        public string Inns { get; set; }
+        public string Mat { get; set; }
+
+    }
+
+    public class T20Is
+    {
+        [DataMember(Name = "10")]
+        public string ten { get; set; }
+        [DataMember(Name = "5w")]
+        public string fiveW { get; set; }
+        [DataMember(Name = "4w")]
+        public string SR { get; set; }
+        public string Econ { get; set; }
+        public string Ave { get; set; }
+        public string BBM { get; set; }
+        public string BBI { get; set; }
+        public string Wkts { get; set; }
+        public string Runs { get; set; }
+        public string Balls { get; set; }
+        public string Inns { get; set; }
+        public string Mat { get; set; }
+
+    }
+
+    public class ODIs
+    {
+        [DataMember(Name = "10")]
+        public string ten { get; set; }
+        [DataMember(Name = "5w")]
+        public string fiveW { get; set; }
+        [DataMember(Name = "4w")]
+        public string SR { get; set; }
+        public string Econ { get; set; }
+        public string Ave { get; set; }
+        public string BBM { get; set; }
+        public string BBI { get; set; }
+        public string Wkts { get; set; }
+        public string Runs { get; set; }
+        public string Balls { get; set; }
+        public string Inns { get; set; }
+        public string Mat { get; set; }
+
+    }
+
+    public class Tests
+    {
+        [DataMember(Name = "10")]
+        public string ten { get; set; }
+        [DataMember(Name = "5w")]
+        public string fiveW { get; set; }
+        [DataMember(Name = "4w")]
+        public string SR { get; set; }
+        public string Econ { get; set; }
+        public string Ave { get; set; }
+        public string BBM { get; set; }
+        public string BBI { get; set; }
+        public string Wkts { get; set; }
+        public string Runs { get; set; }
+        public string Balls { get; set; }
+        public string Inns { get; set; }
+        public string Mat { get; set; }
+
+    }
+
+   
+
+    public class ListA2
+    {
+        [DataMember(Name = "50")]
+        public string fifty { get; set; }
+        [DataMember(Name = "100")]
+        public string oneHundred { get; set; }
+        public string St { get; set; }
+        public string Ct { get; set; }
+        [DataMember(Name = "6s")]
+        public string sixes { get; set; }
+        [DataMember(Name = "4s")]
+        public string fours { get; set; }
+        public string SR { get; set; }
+        public string BF { get; set; }
+        public string Ave { get; set; }
+        public string HS { get; set; }
+        public string Runs { get; set; }
+        public string NO { get; set; }
+        public string Inns { get; set; }
+        public string Mat { get; set; }
+
+    }
+
+    public class FirstClass2
+    {
+        [DataMember(Name = "50")]
+        public string fifty { get; set; }
+        [DataMember(Name = "100")]
+        public string oneHundred { get; set; }
+        public string St { get; set; }
+        public string Ct { get; set; }
+        [DataMember(Name = "6s")]
+        public string sixes { get; set; }
+        [DataMember(Name = "4s")]
+        public string fours { get; set; }
+        public string SR { get; set; }
+        public string BF { get; set; }
+        public string Ave { get; set; }
+        public string HS { get; set; }
+        public string Runs { get; set; }
+        public string NO { get; set; }
+        public string Inns { get; set; }
+        public string Mat { get; set; }
+
+    }
+
+    public class T20Is2
+    {
+        [DataMember(Name = "50")]
+        public string fifty { get; set; }
+        [DataMember(Name = "100")]
+        public string oneHundred { get; set; }
+        public string St { get; set; }
+        public string Ct { get; set; }
+        [DataMember(Name = "6s")]
+        public string sixes { get; set; }
+        [DataMember(Name = "4s")]
+        public string fours { get; set; }
+        public string SR { get; set; }
+        public string BF { get; set; }
+        public string Ave { get; set; }
+        public string HS { get; set; }
+        public string Runs { get; set; }
+        public string NO { get; set; }
+        public string Inns { get; set; }
+        public string Mat { get; set; }
+
+    }
+
+    public class ODIs2
+    {
+        [DataMember(Name = "50")]
+        public string fifty { get; set; }
+        [DataMember(Name = "100")]
+        public string oneHundred { get; set; }
+        public string St { get; set; }
+        public string Ct { get; set; }
+        [DataMember(Name = "6s")]
+        public string sixes { get; set; }
+        [DataMember(Name = "4s")]
+        public string fours { get; set; }
+        public string SR { get; set; }
+        public string BF { get; set; }
+        public string Ave { get; set; }
+        public string HS { get; set; }
+        public string Runs { get; set; }
+        public string NO { get; set; }
+        public string Inns { get; set; }
+        public string Mat { get; set; }
+
+    }
+
+    public class Tests2
+    {
+        [DataMember(Name = "50")]
+        public string fifty { get; set; }
+        [DataMember(Name = "100")]
+        public string oneHundred { get; set; }
+        public string St { get; set; }
+        public string Ct { get; set; }
+        [DataMember(Name = "6s")]
+        public string sixes { get; set; }
+        [DataMember(Name = "4s")]
+        public string fours { get; set; }
+        public string SR { get; set; }
+        public string BF { get; set; }
+        public string Ave { get; set; }
+        public string HS { get; set; }
+        public string Runs { get; set; }
+        public string NO { get; set; }
+        public string Inns { get; set; }
+        public string Mat { get; set; }
+
+    }
+
+    
+}
