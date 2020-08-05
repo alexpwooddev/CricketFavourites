5e6a39e05b1d07ddac883b3bb30e552558be1078
Alex Wood <alexpwooddev@gmail.com>
2020-08-05 16:43:36 -0500

    refactor and clean Favourite Controller

--- Controllers/FavouriteController.cs
+++ Controllers/FavouriteController.cs
@@ -128,75 +128,101 @@ namespace CricketFavourites.Controllers
         [ActionName("CompareWithPlayerSelected")]
         public async Task<IActionResult> CompareStats(int pid1, int pid2)
         {
-            //has just chosen player 1 - no player 2 selection has been made
-            if (pid1 != 0 && pid2 == 0 && (!TempData.ContainsKey("pid2Chosen") || TempData["pid2Chosen"].ToString() == "no"))
+            bool Player1JustChosenPlayer2NotChosen = pid1 != 0 && pid2 == 0 && (!TempData.ContainsKey("pid2Chosen") || TempData["pid2Chosen"].ToString() == "no");
+            bool Player2JustChosenPlayer1NotChosen = pid1 == 0 && pid2 != 0 && (!TempData.ContainsKey("pid1Chosen") || TempData["pid1Chosen"].ToString() == "no");
+            bool Player2JustChosenAfterAnyPreviousSelection = TempData.ContainsKey("pid1Chosen") && TempData["pid1Chosen"].ToString() != "no" && pid2 != 0;
+            bool Player1JustChosenAfterAnyPreviousSelection = TempData.ContainsKey("pid2Chosen") && TempData["pid2Chosen"].ToString() != "no" && pid1 != 0;
+
+            if (Player1JustChosenPlayer2NotChosen)
+            {    
+                List<PlayerInfo> combinedPlayerInfo = await ShowPlayer1ChoiceWithNoPlayer2Choice(pid1);
+                return View("CompareStats", combinedPlayerInfo);
+            }
+       
+            else if (Player2JustChosenPlayer1NotChosen)
             {
-                PlayerInfo firstSelectedPlayerInfo = await _cricApiRepository.GetPlayerInfo(pid1);
-                PlayerInfo secondSelectedPlayerInfo = new PlayerInfo();
-
-                List<PlayerInfo> combinedPlayerInfo = new List<PlayerInfo>();
-                combinedPlayerInfo.Add(firstSelectedPlayerInfo);
-                combinedPlayerInfo.Add(secondSelectedPlayerInfo);
-
-                TempData["pid1Chosen"] = pid1.ToString();
-                TempData["pid2Chosen"] = "no";
-                TempData.Keep();
-
+                List<PlayerInfo> combinedPlayerInfo = await ShowPlayer2ChoiceWithNoPlayer1Choice(pid2);
                 return View("CompareStats", combinedPlayerInfo);
             }
 
-            //has just chosen player 2 - no player 1 selection has been made
-            else if (pid1 == 0 && pid2 != 0 && (!TempData.ContainsKey("pid1Chosen") || TempData["pid1Chosen"].ToString() == "no"))
+            else if (Player2JustChosenAfterAnyPreviousSelection)
             {
-                PlayerInfo secondSelectedPlayerInfo = await _cricApiRepository.GetPlayerInfo(pid2);
-                PlayerInfo firstSelectedPlayerInfo = new PlayerInfo();
-
-                List<PlayerInfo> combinedPlayerInfo = new List<PlayerInfo>();
-                combinedPlayerInfo.Add(firstSelectedPlayerInfo);
-                combinedPlayerInfo.Add(secondSelectedPlayerInfo);
-
-                TempData["pid1Chosen"] = "no";
-                TempData["pid2Chosen"] = pid2.ToString();
-                TempData.Keep();
-
+                List<PlayerInfo> combinedPlayerInfo = await ShowPlayer2ChoiceAndPreviousChoice(pid1, pid2);
                 return View("CompareStats", combinedPlayerInfo);
             }
 
-            //having previously made a selection of any type, has now selected player 2
-            else if (TempData["pid1Chosen"].ToString() != "no" && pid2 != 0)
+            else // if (Player1JustChosenAfterAnyPreviousSelection)
             {
-                pid1 = Int32.Parse(TempData["pid1Chosen"] as string);
-                PlayerInfo firstSelectedPlayerInfo = await _cricApiRepository.GetPlayerInfo(pid1);
-                PlayerInfo secondSelectedPlayerInfo = await _cricApiRepository.GetPlayerInfo(pid2);
-
-                List<PlayerInfo> combinedPlayerInfo = new List<PlayerInfo>();
-                combinedPlayerInfo.Add(firstSelectedPlayerInfo);
-                combinedPlayerInfo.Add(secondSelectedPlayerInfo);
-
-                TempData["pid2Chosen"] = pid2.ToString();
-                TempData["pid1Chosen"] = pid1.ToString();
-                TempData.Keep();
-
+                List<PlayerInfo> combinedPlayerInfo = await ShowPlayer1ChoiceAndPreviousChoice(pid1, pid2);
                 return View("CompareStats", combinedPlayerInfo);
             }
+        }
 
-            //having previously made a selection of any type has now selected Player 1
-            else //(TempData["pid2Chosen"].ToString() != "no" && pid1 != 0)
-            {
-                pid2 = Int32.Parse(TempData["pid2Chosen"] as string);
-                PlayerInfo firstSelectedPlayerInfo = await _cricApiRepository.GetPlayerInfo(pid1);
-                PlayerInfo secondSelectedPlayerInfo = await _cricApiRepository.GetPlayerInfo(pid2);
 
-                List<PlayerInfo> combinedPlayerInfo = new List<PlayerInfo>();
-                combinedPlayerInfo.Add(firstSelectedPlayerInfo);
-                combinedPlayerInfo.Add(secondSelectedPlayerInfo);
+        private async Task<List<PlayerInfo>> ShowPlayer1ChoiceWithNoPlayer2Choice(int pid1)
+        {
+            PlayerInfo firstSelectedPlayerInfo = await _cricApiRepository.GetPlayerInfo(pid1);
+            PlayerInfo secondSelectedPlayerInfo = new PlayerInfo();
 
-                TempData["pid2Chosen"] = pid2.ToString();
-                TempData["pid1Chosen"] = pid1.ToString();
-                TempData.Keep();
+            List<PlayerInfo> combinedPlayerInfo = new List<PlayerInfo>();
+            combinedPlayerInfo.Add(firstSelectedPlayerInfo);
+            combinedPlayerInfo.Add(secondSelectedPlayerInfo);
 
-                return View("CompareStats", combinedPlayerInfo);
-            }
+            TempData["pid1Chosen"] = pid1.ToString();
+            TempData["pid2Chosen"] = "no";
+            TempData.Keep();
+
+            return combinedPlayerInfo;
+        }
+
+        private async Task<List<PlayerInfo>> ShowPlayer2ChoiceWithNoPlayer1Choice(int pid2)
+        {
+            PlayerInfo secondSelectedPlayerInfo = await _cricApiRepository.GetPlayerInfo(pid2);
+            PlayerInfo firstSelectedPlayerInfo = new PlayerInfo();
+
+            List<PlayerInfo> combinedPlayerInfo = new List<PlayerInfo>();
+            combinedPlayerInfo.Add(firstSelectedPlayerInfo);
+            combinedPlayerInfo.Add(secondSelectedPlayerInfo);
+
+            TempData["pid1Chosen"] = "no";
+            TempData["pid2Chosen"] = pid2.ToString();
+            TempData.Keep();
+
+            return combinedPlayerInfo;
+        }
+
+        private async Task<List<PlayerInfo>> ShowPlayer2ChoiceAndPreviousChoice(int pid1, int pid2)
+        {
+            pid1 = Int32.Parse(TempData["pid1Chosen"] as string);
+            PlayerInfo firstSelectedPlayerInfo = await _cricApiRepository.GetPlayerInfo(pid1);
+            PlayerInfo secondSelectedPlayerInfo = await _cricApiRepository.GetPlayerInfo(pid2);
+
+            List<PlayerInfo> combinedPlayerInfo = new List<PlayerInfo>();
+            combinedPlayerInfo.Add(firstSelectedPlayerInfo);
+            combinedPlayerInfo.Add(secondSelectedPlayerInfo);
+
+            TempData["pid2Chosen"] = pid2.ToString();
+            TempData["pid1Chosen"] = pid1.ToString();
+            TempData.Keep();
+
+            return combinedPlayerInfo;
+        }
+
+        private async Task<List<PlayerInfo>> ShowPlayer1ChoiceAndPreviousChoice(int pid1, int pid2)
+        {
+            pid2 = Int32.Parse(TempData["pid2Chosen"] as string);
+            PlayerInfo firstSelectedPlayerInfo = await _cricApiRepository.GetPlayerInfo(pid1);
+            PlayerInfo secondSelectedPlayerInfo = await _cricApiRepository.GetPlayerInfo(pid2);
+
+            List<PlayerInfo> combinedPlayerInfo = new List<PlayerInfo>();
+            combinedPlayerInfo.Add(firstSelectedPlayerInfo);
+            combinedPlayerInfo.Add(secondSelectedPlayerInfo);
+
+            TempData["pid2Chosen"] = pid2.ToString();
+            TempData["pid1Chosen"] = pid1.ToString();
+            TempData.Keep();
+
+            return combinedPlayerInfo;
         }
     }
 }
--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -14,6 +14,8 @@ namespace CricketFavourites.Controllers
     {
         private readonly INewsApiRepository _newsApiRepository;
         private readonly IFavouriteRepository _favouriteRepository;
+        private const int NewsItemsThresholdWhen2FavouritesExist = 2;
+        private const int NewsItemsThresholdWhen3OrMoreFavouritesExist = 1;
 
         public NewsController(INewsApiRepository newsApiRepository, IFavouriteRepository favouriteRepository)
         {
@@ -34,11 +36,12 @@ namespace CricketFavourites.Controllers
             }
 
             //user has no favourited players
-            if (currentUserFavourites.Count() == 0)
+            if (currentUserFavourites.Count == 0)
             {
                 TempData["noFavouritesMessage"] = "You have no favourited players. Please search for players and add them to your favourites.";
                 return View("NewsList");
             }
+
 
             //user only has 1 favourited player
             else if (currentUserFavourites.Count == 1)
@@ -46,10 +49,10 @@ namespace CricketFavourites.Controllers
                 return RedirectToAction("NewsForSelectedPlayer", new { name = currentFavouritePlayerNames[0] });
             }
 
+
             //user has 2 favourited players
             else if (currentUserFavourites.Count == 2)
             {
-                //NewsForTwoPlayers(currentUserFavourites, currentFavouritePlayerNames, newsItemsList, newsItems);
                 //get news items for 2 favourite players into a list
                 for (int i = 0; i < 2; i++)
                 {
@@ -57,9 +60,7 @@ namespace CricketFavourites.Controllers
                     newsItemsList.Add(newsItems);
                 }
 
-                //take first 2 results from each newsItems.items for each player and add to new news
-                //create a blank NewsItems object, then add things to its "List<item> items"? Then return that to the view?
-
+                //take first 2 results from each newsItems.items for each player and add to new List
                 NewsItems combinedItems = new NewsItems();
                 combinedItems.items = new List<Item>();
 
@@ -70,30 +71,35 @@ namespace CricketFavourites.Controllers
 
                 for (int i = 0; i < 2; i++)
                 {
-                    if (newsItemsList[i].items.Count() < 2)
+                    if (newsItemsList[i].items.Count() < NewsItemsThresholdWhen2FavouritesExist)
                     {
                         playerHasTwoItems[i] = false;
                     }
                 }
+          
+                bool notEnoughResultsForEitherPlayer = playerHasTwoItems[0] == false && playerHasTwoItems[1] == false;
+                bool onlyPlayer1HasEnoughResults = playerHasTwoItems[0] == true && playerHasTwoItems[1] == false;
+                bool onlyPlayer2HasEnoughResults = playerHasTwoItems[0] == false && playerHasTwoItems[1] == true;
 
-                //if neither player has at least 2 results then display nothing:
-                if (playerHasTwoItems[0] == false && playerHasTwoItems[1] == false)
+
+                //if neither player has at least 2 results then display nothing
+                if (notEnoughResultsForEitherPlayer)
                 {
                     return View("NewsList");
                 }
 
                 //if one player doesn't have at least 2 results, then just display results for the other:
-                else if (playerHasTwoItems[0] == true && playerHasTwoItems[1] == false)
+                else if (onlyPlayer1HasEnoughResults)
                 {
                     return RedirectToAction("NewsForSelectedPlayer", new { name = currentFavouritePlayerNames[0] });
                 }
 
-                else if (playerHasTwoItems[0] == false && playerHasTwoItems[1] == true)
+                else if (onlyPlayer2HasEnoughResults)
                 {
                     return RedirectToAction("NewsForSelectedPlayer", new { name = currentFavouritePlayerNames[1] });
                 }
 
-                else
+                else //display 2 results for each player
                 {
                     for (int i = 0; i < 2; i++)
                     {
@@ -113,12 +119,20 @@ namespace CricketFavourites.Controllers
             //user has 3 or more favourited players
             else
             {
-                //check that each player actually has at least 1 result
                 Dictionary<int, bool> playerHasOneItem = new Dictionary<int, bool>();
                 playerHasOneItem.Add(0, true);
                 playerHasOneItem.Add(1, true);
                 playerHasOneItem.Add(2, true);
 
+                bool noPlayerHasResult = playerHasOneItem[0] == false && playerHasOneItem[1] == false && playerHasOneItem[2] == false;
+                bool onlyPlayer1HasResults = playerHasOneItem[0] == true && playerHasOneItem[1] == false && playerHasOneItem[2] == false;
+                bool onlyPlayer2HasResults = playerHasOneItem[0] == false && playerHasOneItem[1] == true && playerHasOneItem[2] == false;
+                bool onlyPlayer3HasResults = playerHasOneItem[0] == false && playerHasOneItem[1] == false && playerHasOneItem[2] == true;
+                bool onlyPlayer1HasNoResults = playerHasOneItem[0] == false && playerHasOneItem[1] == true && playerHasOneItem[2] == true;
+                bool onlyPlayer2HasNoResults = playerHasOneItem[0] == true && playerHasOneItem[1] == false && playerHasOneItem[2] == true;
+                bool onlyPlayer3HasNoResults = playerHasOneItem[0] == true && playerHasOneItem[1] == true && playerHasOneItem[2] == false;
+
+                //check if each player has news items
                 for (int i = 0; i < 3; i++)
                 {
                     newsItems = _newsApiRepository.GetNews(currentFavouritePlayerNames[i]);
@@ -127,47 +141,47 @@ namespace CricketFavourites.Controllers
 
                 for (int i = 0; i < 3; i++)
                 {
-                    if (newsItemsList[i].items.Count() < 1)
+                    if (newsItemsList[i].items.Count() < NewsItemsThresholdWhen3OrMoreFavouritesExist)
                     {
                         playerHasOneItem[i] = false;
                     }
                 }
+                
 
-                //if no player has at least 1 result then display nothing:
-                if (playerHasOneItem[0] == false && playerHasOneItem[1] == false && playerHasOneItem[2] == false)
+                if (noPlayerHasResult)
                 {
                     return View("NewsList");
                 }
 
-                //if only 1 player has at least 1 result, then just display results for that player
-                else if (playerHasOneItem[0] == true && playerHasOneItem[1] == false && playerHasOneItem[2] == false)
+                //if only 1 player has at least 1 result, then just display results for that player            
+                else if (onlyPlayer1HasResults)
                 {
                     return RedirectToAction("NewsForSelectedPlayer", new { name = currentFavouritePlayerNames[0] });
                 }
 
-                else if (playerHasOneItem[0] == false && playerHasOneItem[1] == true && playerHasOneItem[2] == false)
+                else if (onlyPlayer2HasResults)
                 {
                     return RedirectToAction("NewsForSelectedPlayer", new { name = currentFavouritePlayerNames[1] });
                 }
 
-                else if (playerHasOneItem[0] == false && playerHasOneItem[1] == false && playerHasOneItem[2] == true)
+                else if (onlyPlayer3HasResults)
                 {
                     return RedirectToAction("NewsForSelectedPlayer", new { name = currentFavouritePlayerNames[2] });
                 }
 
 
                 //if two players have at least 1 result, but the 3rd has none, just display results for one of the players who does have some
-                else if (playerHasOneItem[0] == true && playerHasOneItem[1] == true && playerHasOneItem[2] == false)
+                else if (onlyPlayer3HasNoResults)
                 {
                     return RedirectToAction("NewsForSelectedPlayer", new { name = currentFavouritePlayerNames[0] });
                 }
 
-                else if (playerHasOneItem[0] == true && playerHasOneItem[1] == false && playerHasOneItem[2] == true)
+                else if (onlyPlayer2HasNoResults)
                 {
                     return RedirectToAction("NewsForSelectedPlayer", new { name = currentFavouritePlayerNames[2] });
                 }
 
-                else if (playerHasOneItem[0] == false && playerHasOneItem[1] == true && playerHasOneItem[2] == true)
+                else if (onlyPlayer1HasNoResults)
                 {
                     return RedirectToAction("NewsForSelectedPlayer", new { name = currentFavouritePlayerNames[1] });
                 }
@@ -175,10 +189,7 @@ namespace CricketFavourites.Controllers
 
                 else
                 {
-                    //if all 3 players have at least 1 result
-                    //take first result from each newsItems.items for each player and add to new news
-                    //create a blank NewsItems object, then add things to its "List<item> items"? Then return that to the view?
-
+                    //if all 3 players have at least 1 result, take first result from each newsItems.items for each player
                     NewsItems combinedItems = new NewsItems();
                     combinedItems.items = new List<Item>();
 
@@ -191,7 +202,6 @@ namespace CricketFavourites.Controllers
                     }
 
                     TempData["newsPageTitle"] = "Recent news related to some of your favourite players";
-
                     return View("NewsList", combinedItems);
                 }
             }
